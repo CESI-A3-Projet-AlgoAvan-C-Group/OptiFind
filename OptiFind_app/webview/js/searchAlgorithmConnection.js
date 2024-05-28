c2ae3c6c267781fb1498409457e692e47d6fa80a
lukashouille <lukas.houille@viacesi.fr>
2024-05-28 12:45:35 +0200

    feat: send truck groups and package groups to back end

--- OptiFind_app/webview/js/searchAlgorithmConnection.js
+++ OptiFind_app/webview/js/searchAlgorithmConnection.js
@@ -1,32 +1,34 @@
-function searchRequest() {
+async function searchRequest() {
     changeToResults();
     if (document.cookie.includes('mode=manual')) {
-        let truckGroups = getGroups('truckGroups');
-        let packageGroups = getGroups('packageGroups');
+        let truckGroups = await getGroups('truckGroups');
+        let packageGroups = await getGroups('packageGroups');
 
         // send request to flask search algorithm
-        getPaths('manual',truckGroups, packageGroups);
+        getPaths('manual', truckGroups, packageGroups);
     } else {
         // send request to flask search algorithm
         getPaths('assisted');
     }
 }
 
-function getPaths(searchMode,truckGroups, packageGroups) {
+function getPaths(searchMode, truckGroups, packageGroups) {
+    console.log(truckGroups);
+    console.log(packageGroups);
     fetch('/get_paths', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            truckGroups,
-            packageGroups,
-            mapData: map.getSource('uploaded-source')._data
+            searchMode: searchMode,
+            truckGroups: truckGroups,
+            packageGroups: packageGroups
         })
     }).then(response => response.json())
         .then(data => {
-            showPaths(data);
-        }
+                showPaths(data);
+            }
         );
 }
 
@@ -37,10 +39,14 @@ function backToSearch() {
 }
 
 function getGroups(groupType) {
+    // get the data from index db and return it as an array
     let transaction = db.transaction(groupType, 'readonly');
-    let groups = transaction.objectStore(groupType);
-    let request = groups.getAll();
-    request.onsuccess = function () {
-        return request.result;
-    }
+    let Groups = transaction.objectStore(groupType);
+    let request = Groups.getAll();
+    // wait request to finish and return the result
+    return new Promise(resolve => {
+        request.onsuccess = function () {
+            resolve(request.result);
+        }
+    });
 }
