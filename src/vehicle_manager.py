6dcf44bb08aabd78300b5acac9cb468cc1521ed9
Roooomain <rommor2810@gmail.com>
2024-05-28 14:01:07 +0200

    feat : add a new best fist with a score function

--- src/vehicle_manager.py
+++ src/vehicle_manager.py
@@ -1,3 +1,6 @@
+from math import radians, sin, cos, sqrt, atan2
+
+
 class Vehicle:
     def __init__(self, vehicle_id, capacity, volume):
         self.id = vehicle_id
@@ -19,12 +22,90 @@ class Vehicle:
             self.remaining_capacity += package.weight
             self.remaining_volume += package.volume
 
+    def calculate_packages_center(self):
+        if not self.packages:
+            return 48.866667, 2.333333
+        total_latitude = sum(package.latitude for package in self.packages)
+        total_longitude = sum(package.longitude for package in self.packages)
+        count = len(self.packages)
+        return [total_latitude / count, total_longitude / count]
+
+
 class Package:
-    def __init__(self, package_id, weight, volume, coordinate):
+    def __init__(self, package_id, weight, volume, latitude, longitude):
         self.id = package_id
         self.weight = weight
         self.volume = volume
-        self.coordinate = coordinate
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def calculate_distance(self, reference_position):
+        lat1 = radians(self.latitude)
+        lon1 = radians(self.longitude)
+        lat2 = radians(reference_position[0])
+        lon2 = radians(reference_position[1])
+
+        R = 6371.0
+
+        dlat = abs(lat2 - lat1)
+        dlon = abs(lon2 - lon1)
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = R * c
+
+        return distance
+
+
+def calculate_score(package, vehicle, max_distance=500):
+
+    center_position = vehicle.calculate_packages_center()
+    distance = package.calculate_distance(center_position)
+
+    if distance > max_distance and (len(vehicle.packages) != 0):
+        return -1
+
+    if distance > max_distance and (len(vehicle.packages) == 0):
+        max_distance = distance
+
+    score = ((package.weight / vehicle.remaining_capacity) + (package.volume / vehicle.remaining_volume) + ((1 - (distance / max_distance)) * 1.2))
+    return score
+
+
+def best_fit_decreasing_score(packages, vehicles):
+    """
+        Attribue les colis aux véhicules en utilisant le principe d'un algorithme BFD avec une fonction score.
+
+        Args:
+        packages (list of Package): Liste d'objets Package représentant les colis à distribuer.
+        vehicles (list of Vehicle): Liste d'objets Vehicle représentant les véhicules disponibles.
+
+        Returns:
+        vehicles (list of Vehicle): Liste d'objets Vehicle avec leur colis (Vehicle.packages).
+        packages_left (list of Package): Liste d'objets Package qui n'ont pas été distribués.
+    """
+
+    sorted_packages = sorted(packages, key=lambda x: (x.weight, x.volume), reverse=True)
+    packages_left = []
+
+    for package in sorted_packages:
+        best_fit_vehicle = None
+        best_score = float(0)
+
+        for vehicle in vehicles:
+            if vehicle.remaining_capacity >= package.weight and vehicle.remaining_volume >= package.volume:
+                score = calculate_score(package, vehicle)
+                if score > best_score:
+                    best_score = score
+                    best_fit_vehicle = vehicle
+
+        if best_fit_vehicle is None:
+            packages_left.append(package)
+            continue
+
+        best_fit_vehicle.add_package(package)
+
+    return vehicles, packages_left
 
 def best_fit_decreasing(packages, vehicles):
     """
