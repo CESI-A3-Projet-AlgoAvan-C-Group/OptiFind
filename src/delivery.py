727c3c74e80563148b161cfea3efe8ba4efb0484
RitterAntoine <antoine.ritter@viacesi.fr>
2024-05-31 11:43:27 +0200

    feat: geojson ready for front

--- src/delivery.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class Delivery:
-    def __init__(self, name, coordinates, is_start=False):
-        self.name = name
-        self.coordinates = coordinates
-        self.is_start = is_start
