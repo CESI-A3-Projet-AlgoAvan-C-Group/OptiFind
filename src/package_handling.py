f9c7ef65b52d9641b0f97e781fc43a0f04c59466
lukashouille <lukas.houille@viacesi.fr>
2024-05-31 09:14:58 +0200

    feat: package distribution

---
+++ src/package_handling.py
@@ -0,0 +1,83 @@
+from src.vehicle_manager import Vehicle, Package, best_fit_decreasing_score
+import geopandas as gpd
+import random
+import json
+
+def create_features_from_pkg(packages):
+    features = []
+
+    for package in packages:
+        package_feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [package.longitude, package.latitude]
+            },
+            "properties": {
+                "package_id": package.id,
+                "weight": package.weight,
+                "volume": package.volume,
+                "city": package.city
+            }
+        }
+
+        features.append(package_feature)
+
+    return features
+
+
+def extract_vehicles(truckGroups):
+    vehicles = []
+    for truck_index, truck in enumerate(truckGroups):
+        print(truck)
+        for i in range(int(truck['quantity'])):
+            vehicle_id = (truck_index+1) * 10000 + i + 1
+            vehicle = Vehicle(
+                vehicle_id=vehicle_id,
+                capacity=float(truck['weight']),
+                volume=float(truck['volume']))
+            vehicles.append(vehicle)
+
+    return vehicles
+
+
+def get_random_city(communes):
+    random_number = random.randrange(0, len(communes))
+
+    return communes.iloc[random_number]
+
+def extract_packages_for_paths(mapData):
+    packages = []
+    for feature in mapData['features']:
+        properties = feature['properties']
+        geometry = feature['geometry']
+        package = Package(
+            package_id=properties['package_id'],
+            weight=float(properties['weight']),
+            volume=float(properties['volume']),
+            latitude=geometry['coordinates'][1],
+            longitude=geometry['coordinates'][0],
+            city=properties['city']
+        )
+        packages.append(package)
+    return packages
+
+def extract_packages_with_random_city(packageGroups):
+    packages = []
+    communes = gpd.read_file('../assets/data/centre_communes.geojson')
+
+    for pkg_index, pkg in enumerate(packageGroups):
+        for i in range(int(pkg['quantity'])):
+            ville = get_random_city(communes)
+            package_id = (pkg_index+1) * 100000 + i + 1
+            package = Package(
+                package_id= package_id,
+                weight=float(pkg['weight']),
+                volume=float(pkg['volume']),
+                latitude=ville.geometry.y,
+                longitude=ville.geometry.x,
+                city=ville.nom_officiel)
+
+            packages.append(package)
+
+    return packages
